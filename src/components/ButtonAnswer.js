37621b368173d71cadb162589ae4f5108902f669
Colby Fayock <fayock@gmail.com>
2023-05-04 09:21:51 -0400

    dark mode

--- src/components/ButtonAnswer.js
+++ src/components/ButtonAnswer.js
@@ -3,7 +3,7 @@ const ButtonAnswer = ({ children, answerState, ...rest}) => (
     className={`
       w-full h-full
       text-lg uppercase font-bold
-      bg-white hover:bg-yellow-300 data-[answer-state=right]:bg-green-300 data-[answer-state=wrong]:bg-red-300
+      bg-white dark:bg-slate-500 hover:bg-yellow-300 data-[answer-state=right]:bg-green-300 data-[answer-state=wrong]:bg-red-300
       border-4 border-cyan-300 hover:border-yellow-500 data-[answer-state=right]:border-green-500 data-[answer-state=wrong]:border-red-500
       p-16 rounded
     `}
